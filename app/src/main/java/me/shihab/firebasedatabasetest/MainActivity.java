5c1bcbcd7c0a34d3a39a75e37de9e092f7db1034
DESKTOP-53A39RD\User <shihab.apps.developer@gmail.com>
2018-02-01 16:08:56 +0600

    Some Changes

--- app/src/main/java/me/shihab/firebasedatabasetest/MainActivity.java
+++ app/src/main/java/me/shihab/firebasedatabasetest/MainActivity.java
@@ -27,15 +27,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         button = findViewById(R.id.button);
 
         // Create database instance
